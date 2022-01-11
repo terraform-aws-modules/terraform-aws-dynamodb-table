ded66ee7ef7630e0d862a98c497d245d0cc4c829
Max Williams <max-rocket-internet@users.noreply.github.com>
2022-01-11 10:07:26 +0100

    remove a var that is not needed

--- outputs.tf
+++ outputs.tf
@@ -10,10 +10,10 @@ output "dynamodb_table_id" {
 
 output "dynamodb_table_stream_arn" {
   description = "The ARN of the Table Stream. Only available when var.stream_enabled is true"
-  value       = var.stream_enabled ? concat(local.table_in_use.*.stream_arn, [""])[0] : null
+  value       = var.stream_enabled ? try(aws_dynamodb_table.this[0].id, aws_dynamodb_table.autoscaled[0].stream_arn, "") : null
 }
 
 output "dynamodb_table_stream_label" {
   description = "A timestamp, in ISO 8601 format of the Table Stream. Only available when var.stream_enabled is true"
-  value       = var.stream_enabled ? concat(local.table_in_use.*.stream_label, [""])[0] : null
+  value       = var.stream_enabled ? try(aws_dynamodb_table.this[0].id, aws_dynamodb_table.autoscaled[0].stream_label, "") : null
 }
